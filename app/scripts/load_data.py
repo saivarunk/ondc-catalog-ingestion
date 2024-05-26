e19336449fc8c62f16f0d8b7aa75a626172dd983
Varun Kruthiventi <varun@skyhive.io>
2024-05-26 10:58:28 +0530

    - Updated templates

--- app/scripts/load_data.py
+++ app/scripts/load_data.py
@@ -5,7 +5,8 @@ from tqdm import tqdm
 
 import pandas as pd
 
-backend_host = "http://localhost"
+# backend_host = "http://localhost"
+backend_host = "http://34.93.187.143/"
 
 # Read the CSV file
 csv_file = "app/dataset/BigBasketProducts.csv"
@@ -16,12 +17,12 @@ dataset['rating'] = dataset['rating'].fillna(0)
 dataset['description'] = dataset['description'].fillna("")
 dataset['brand'] = dataset['brand'].fillna("")
 
-documents = dataset.to_dict(orient="records")[:100]
-batch_size = 100
+documents = dataset.to_dict(orient="records")
+batch_size = 1000
 
 for i in tqdm(range(0, len(documents), batch_size)):
     batch = documents[i:i + batch_size]
-    response = requests.post(f"{backend_host}/api/v1/catalogs/6651d410b3fbd938620fdf6f/products", json={
+    response = requests.post(f"{backend_host}/api/v1/catalogs/665230b696518044064f249b/products", json={
         "records": batch,
         "enable_vector_indexing": False
     })
